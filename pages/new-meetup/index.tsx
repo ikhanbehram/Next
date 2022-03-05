0b86dfe5b4f541a0724b8c7f13ba2f603470e87c
Behram <77900065+ikhanbehram@users.noreply.github.com>
2022-03-05 17:51:41 +0500

    initial commit

---
+++ pages/new-meetup/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const NewMeetup: NextPage = () => {
+  return (
+      <>
+    <h1>NEXT NEWS PAGE</h1>
+    <h1>FAST</h1>
+    </>
+  )
+}
+
+export default NewMeetup
